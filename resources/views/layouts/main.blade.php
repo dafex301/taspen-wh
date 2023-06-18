dfcef91ab5580fb76a967ae5c5a9301bd8db29d2
Fahrel <fahrel@students.undip.ac.id>
2023-06-18 19:03:23 +0700

    feat: add input stok to hc&ga sector head

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -145,6 +145,19 @@
                             </li>
                         @else --}}
                         <li class="nav-title">Menu Manajer Bidang</li>
+                        @if (auth()->user()->Bidang->nama === 'Bidang Umum dan SDM')
+                            <li class="nav-item w-100">
+                                <a class="nav-link" href="/umum/stok">
+                                    <svg style="width: 20px; margin: 0 5px 0 5px" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                        class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                                    </svg>
+                                    Input Stok
+                                </a>
+                            </li>
+                        @endif
                         <li class="nav-item w-100"><a class="nav-link" href="/bidang/permintaan/verifikasi">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                                     style="width: 20px; margin: 0 5px 0 5px">
